d075a656e0d42c6340b9b740ebb0dd45f0028e33
Lucas Varela <lucaas.varela@gmail.com>
2018-09-10 00:51:43 -0300

    Initial Commit
    Adicionando classes bases para a busca uniforme
    (Utilizando o codigo do Vilson Heck como partida)
    Criação do form e imagens bases para a simulação
    E um exemplo de grafo(grafo-sample) para testes

---
+++ RobotNavigationUCS/src/main/java/br/edu/ifsc/robotnavigation/model/CustoUniforme.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifsc.robotnavigation.model;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *
+ * @author Lucas
+ */
+public class CustoUniforme {
+    
+    //TODO Adaptar a busca de Dijkstra para busca uniforme
+    public static ArrayList<Vertice> dijkstra(Grafo g, Vertice s) {
+        Queue<Vertice> Q = new LinkedList();
+        ArrayList<Vertice> S = new ArrayList();
+
+        for (Vertice u : g.obterVertices()) {
+            u.setCaminho("");
+            u.zerarDistancia();
+            Q.add(u);
+        }
+        s.setCaminho("");
+        s.definirDistancia(0);
+
+        while (!Q.isEmpty()) {
+            System.out.println(Q);
+            //Pegar da fila
+            Vertice u = Q.remove();
+            
+            //Obtendo os ADJ a partir dos arcos, e colorindo o vertice a partir dos destinos obtidos
+            for (Arco arco : u.obterArcos()) {
+
+                Vertice v = arco.getDestino();
+//                System.out.println("u: " + u + ", v: " + v);
+                if (v.obterDistancia() > u.obterDistancia() + arco.getPeso()) {
+                    //---
+                    v.definirDistancia(u.obterDistancia() + arco.getPeso());
+                    v.setCaminho(u.getCaminho());
+                    //---
+                }
+            }
+            S.add(u);
+        }
+        return S;
+    }
+    
+}
